48c1ed37d7ec7c0666c630370625e262bd71fcb9
YuliyaMinsk <yuliya.rostova@gmail.com>
2023-10-02 22:36:31 +0600

    task: create reminder telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,50 @@
 import TelegramBot from 'node-telegram-bot-api';
 import 'dotenv/config';
 
+interface Note {
+  uid: number;
+  time: string;
+  text: string;
+}
+
 const token: string = process.env.TELEGRAM_BOT_TOKEN || '';
 
 const bot = new TelegramBot(token, { polling: true });
 
-// /echo Hello World! - send message "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg: TelegramBot.Message, match: RegExpExecArray | null) {
-  const fromId = msg.from?.id;
-  const resp = match ? match[1] : '';
-  if (fromId) {
-    bot.sendMessage(fromId, resp);
+let notes: Note[] = [];
+
+bot.onText(/remind (.+) in (.+)/, function (msg: TelegramBot.Message, match: RegExpExecArray | null) {
+  const userId = msg.from?.id;
+  const text = match ? match[1] : '';
+  const time = match ? match[2] : '';
+
+  console.log('GOT INFO: ', userId, text, time);
+
+  if (userId === undefined || text === undefined || time === undefined) {
+    return;
   }
+
+  notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+  bot.sendMessage(userId, 'OK) Got it!');
 });
 
-// send cat for all messages
-bot.on('message', function (msg: TelegramBot.Message) {
-  const chatId = msg.chat.id;
+setInterval(function () {
+  for (var i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+    if (notes[i]['time'] === curDate) {
+      sendCat(notes[i]['uid']);
+      bot.sendMessage(notes[i]['uid'], 'Remain that: ' + notes[i]['text'] + ' now.');
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
+
+function sendCat(chatId: number) {
   const photo = 'src/cat.png';
-  bot.sendPhoto(chatId, photo, { caption: 'Милый кот' });
-});
+  const options = {
+    caption: 'Cute cat',
+    contentType: 'application/octet-stream',
+  };
+  bot.sendPhoto(chatId, photo, options);
+}
